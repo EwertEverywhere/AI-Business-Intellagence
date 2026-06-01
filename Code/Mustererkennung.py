12fcd631e424cec83238903262b025708f8a1012
eewert.ee@googlemail.com <eewert.ee@googlemail.com>
2026-06-01 08:18:34 +0200

    my data

--- Code/Mustererkennung.py
+++ Code/Mustererkennung.py
@@ -41,4 +41,24 @@ plt.ylim(0,8)
 title_plot = "Gruppe C"
 plt.title(title_plot)
 
-plt.show()
+#plt.show()
+
+# Erkennung einer Gruppe aufgrund von 2 Kriterien
+# Die Voranalyse zeigt, dass die 'Gruppe A' auf Grundlage der Werte der Kriterien X3 und X4 eindeutig identifiziert werden kann.
+
+df_identified = df.loc[(df.X3 <= 1.9) & (df.X4 <= 0.6)]
+print('Correct: ', df_identified.Y.count(), '/50')
+
+
+print(Gruppe_B_df.describe())
+print(Gruppe_C_df.describe())
+
+df_idetified_B = df.loc[(df.X1<=7.0) & (df.X2<=3.4) & (df.X3<=5.1) & (df.X4<=1.8)]
+print('Correct: ',df_idetified_B.Y.count(),'/50')
+#df_idetified_three
+
+
+
+
+
+
